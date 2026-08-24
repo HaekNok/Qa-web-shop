f59a863b4ed66ce1b4b38060db98d75d8c381080
HaekNok <superlunie33@gmail.com>
2026-08-25 01:45:40 +0300

    мб завтра доработать уже

---
+++ utils/test_data.py
@@ -0,0 +1,13 @@
+BASE_URL = "https://www.saucedemo.com"
+API_BASE_URL = "https://reqres.in"
+
+VALID_USER = "standard_user"
+VALID_PASSWORD = "secret_sauce"
+
+INVALID_CREDENTIALS = [
+    ("invalid_user", "invalid_password"),
+    ("standard_user", "wrong_password"),
+    ("", ""),
+]
+
+PRODUCT_NAME = "Sauce Labs Backpack"
